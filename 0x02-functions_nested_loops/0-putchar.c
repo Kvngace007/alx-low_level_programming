3b27b826a36cb6cfe8e2ed0976ce345a6f452ff7
Eghosa Alex <alexsparks007@gmail.com>
2023-06-20 16:57:42 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,6 +7,14 @@
 
 int main(void)
  {
-    putchar("_putchar\n");
-    return (0);
+  char school[10] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(school[i]);
+	}
+	_putchar(10);
+	return (0);
 }
