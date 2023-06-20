5e9e077f404b74a2736429c603d7a7f312312d3a
Eghosa Alex <alexsparks007@gmail.com>
2023-06-20 16:20:01 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,13 @@
 #include "main.h"
+
 /**
 *main - function prints text as output
 *
 *Return:return 0
 */
-int main(void) {
-    putchar('_');
-    putchar('p');
-    putchar('u');
-    putchar('t');
-    putchar('c');
-    putchar('h');
-    putchar('a');
-    putchar('r');
-    putchar('\n');
 
+int main(void)
+ {
+    putchar("_putchar\n");
     return (0);
 }
