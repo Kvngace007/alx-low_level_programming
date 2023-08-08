6a4cb3de955295095f8d9e41e4e0fe09cdd423af
Eghosa Alex <alexsparks007@gmail.com>
2023-08-08 13:35:23 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	d = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(d, text_content, len);
 
-	if (fd == -1 || w == -1)
+	if (d == -1 || w == -1)
 		return (-1);
 
 	close(d);
